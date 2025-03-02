07c982d1c8e35eca76d827ebbd9cb2db32499f12
Lokesh Kumar Thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2025-03-02 08:53:44 +0530

    Code to start appium server programatically added

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,17 +1,40 @@
 package testRunner;
 
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 import wrappers.GenericWrappers;
 
 @CucumberOptions(features = "src/test/resources/features",
         glue = "pages", publish = true)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    public AppiumServiceBuilder builder;
+    public AppiumDriverLocalService service;
+
+    @BeforeSuite
+    public void startAppiumServer() {
+        builder = new AppiumServiceBuilder();
+        builder.usingPort(4723);
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.ALLOW_INSECURE, "chromedriver_autodownload");
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+        System.out.println("Appium server started.");
+    }
+
+    @AfterSuite
+    public void stopAppiumServer() {
+        if (service != null && service.isRunning()) {
+            service.stop();
+            System.out.println("Appium server stopped.");
+        }
+    }
+
     @Parameters({"app", "appPackage", "appActivity", "udid", "systemPort"})
     @BeforeMethod
     public void bm(String app, String appPackage, String appActivity, @Optional("") String udid, @Optional("") String systemPort) {
